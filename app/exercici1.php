db87139fb9b5eb17b510368417774a89cb551bbf
iaw47714462 <iaw47714462@h16.informatica.escoladeltreball.org>
2021-09-27 20:05:21 +0200

    segona practica

--- app/exercici1.php
+++ app/exercici1.php
@@ -1,27 +1,79 @@
-<?php 
-$A = 1;
-$B = 5 ;
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <?php 
+$A = 2;
+$B = 4 ;
+$resul
 ?>
+        <h5 class="h5">Pildora 1: Syntaxis
+        </h5>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td scope="col">Operacion</td>
+                    <td scope="col">valor</td>
+                </tr>
+            </thead>
+            <tr scope="row">
+                <td>A</td>
+                <td><?php echo$A?> </td>
 
-<div class="container">
-    <table>
-        <thead>
-            <tr>
-                <td>Operacion</td>
-                <td>valor</td>
             </tr>
-        </thead>
-        <tr>
+            <tr scope="row">
+                <td>B</td>
+                <td><?php echo$B?> </td>
+            </tr>
 
-            <td><?php echo $A?></td>
-            <td><?php echo $A?></td>
-        </tr>
-        <tr>
-            <td><?php echo $B?></td>
-            <td><?php echo $B?></td>
+            <tr scope="col">
+                <td>A+B</td>
+                <td><?php echo($A+$B) ?> </td>
+            </tr>
+            <tr scope="col">
+                <td>A-B</td>
+                <td><?php echo($A-$B) ?> </td>
+            </tr>
+            <tr scope="col">
+                <td>A*B</td>
+                <td><?php echo($A*$B) ?> </td>
+            </tr>
+            <tr scope="col">
+                <td>A/B</td>
+                <td><?php echo($A/$B) ?> </td>
+            </tr>
+            <tr scope="col">Pildora 1: Syntaxis
+
+                <td>AexpB</td>
+                <td><?php echo(pow($A,$B)) ?> </td>
+            </tr>
+        </table>
+    </div>
+    <div class="container">
+        <h5 class="h5">Pildora 2: Comunicacion entre páginas</h5>
+        <form action="accion.php" method="post">
+            
+            <input class="input-group mb-3" type="text" name="nom" /></p>
+            <p>Email: <input type="email" name="email" /></p>
+            <p><input type="submit" /></p>
+        </form>
+    </div>
+</body>
+
+</html>
 
 
-    </table>
-</div> 
+
+
 
 <?php echo '<h1>Hello World</h1><p>Mi primer programa</p>'; ?>
