dfb3b4e54397885eee273f4c3c928d72ddbe82c3
josep <olmosescolatreball@gmail.com>
2021-10-03 04:17:52 +0200

    chekeando

--- app/components/views/futbol_01.php
+++ app/components/views/futbol_01.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Generar Taula</title>
 </head>
 
 <body>
@@ -15,8 +15,8 @@
                 <tbody>
                     <form action="futbol_01_controller.php" method="post">
                         <tr>
-                            <td>Numero de jugador</td>
-                            <td><input type="number" name="player"> </td>
+                            <!-- <td>Numero de jugador</td>
+                            <td><input type="number" name="player"> </td> -->
                             <td>Numero de partits</td>
                             <td><input type="number" name="match"> </td>
                             <td><button type="submit" class="btn btn-dark">Crear Taula</button> </td>
