00f92e51bb2bc6dfed7448d27ac79b670fa40e10
josep <olmosescolatreball@gmail.com>
2021-10-03 03:55:24 +0200

    començan MCV

---
+++ app/components/views/futbol_01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="/app/components/controller/futbol_01_controller.php" method="post">
+            <table class="table  w-50 table-striped table-dark table-hover ">
+                <tbody>
+                    <form action="futbol_01_controller.php" method="post">
+                        <tr>
+                            <td>Numero de jugador</td>
+                            <td><input type="number" name="player"> </td>
+                            <td>Numero de partits</td>
+                            <td><input type="number" name="match"> </td>
+                            <td><button type="submit" class="btn btn-dark">Crear Taula</button> </td>
+                        </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+</body>
+
+</html>
--- app/index.php
+++ app/index.php
@@ -5,21 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Gols per jugador</title>
 </head>
 
-<body>
-    <form action="insertData.php" method="post">
-        <h5>Generador de tabla</h5>
-        <label for="player">Numero de jugadores:</label>
-        <input type="number" name="player" id="">
-        <label for="match">Numero de partidos</label>
-        <input type="number" name="match" id="">
-        <button type="submit" class="btn btn-primary">Generar</button>
-    </form>
+<body onload="app/components/controllers/futbol_01_controller.phpfutbol_01.php">
 
 </body>
-
 </html>
